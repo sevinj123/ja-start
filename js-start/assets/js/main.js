04e755b47e984589297d835925da1fb025a48a93
sevinj123 <127213193+sevinj123@users.noreply.github.com>
2023-04-11 19:50:37 +0400

    Add files via upload

---
+++ js-start/assets/js/main.js
@@ -0,0 +1,83 @@
+// var num = 60
+// if (num%30==0)
+//  {
+//     alert("eded 3 e ve 10 a bolunur")
+// }
+// else{
+//     alert("eded 3e ve 10 a bolunmur")
+// }
+
+
+
+
+
+
+
+//  var num=45
+//  if (num>90 && num<101 ) 
+//  {
+//     alert("A")
+//  } 
+//  else if (num>80 && num<91)
+//  {
+//     alert (" B")
+//  }
+//  else if (num>70 && num<81) 
+//  {
+//     alert("C")
+//  } 
+//  else if (num>60 && num<71)
+//  {
+//     alert (" D")
+//  }
+//  else if (num>50 && num<61) 
+//  {
+//     alert("E")
+//  } 
+//  else if(num >0 && num < 50){
+//     alert("kesildiniz")
+//  }
+//  else
+//  {
+//     alert (" duzgun deyer daxil edin ")
+//  }
+
+
+
+
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+
+// var x=6 , y=6;
+// if (x%2==0 && y%2==0) 
+// {
+//     alert(x+y)
+// }
+// else{
+//     alert("false")
+// }
+
+
+
+var num x,teklik,onluq;
+if (x>9 && x<100) 
+
+
+
+
+
+
